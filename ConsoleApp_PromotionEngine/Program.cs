ff1cb6daba1cdab4c16d50a43a307c934139f1d9
pradipphopase <71191256+pradipphopase@users.noreply.github.com>
2020-09-13 21:52:09 +0530

    initial commit

---
+++ ConsoleApp_PromotionEngine/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp_PromotionEngine.Model;
+using ConsoleApp_PromotionEngine.Service;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp_PromotionEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           List<Product> products = new List<Product>();
+
+            Console.Write("How many orders?=> ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                Console.Write("{0} - Enter the type of product:A,B,C or D=> ",i+1);
+                string type = Console.ReadLine();
+                Product p = new Product(type);
+                products.Add(p);
+            }
+            Calculation c = new Calculation();
+            int totalPrice = c.GetTotalPrice(products);
+            Console.WriteLine("Total => {0}",totalPrice);
+            Main(args);
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ ConsoleApp_PromotionEngine/Service/Calculation.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleApp_PromotionEngine.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_PromotionEngine.Service
+{
+    public class Calculation
+    {
+        public int GetTotalPrice(List<Product> products)
+        {
+            int counterofA = 0;
+            int priceofA = 50;
+            int counterofB = 0;
+            int priceofB = 30;
+            int CounterofC = 0;
+            int priceofC = 20;
+            int CounterofD = 0;
+            int priceofD = 15;
+            foreach (Product pr in products)
+            {
+                if (pr.Id == "A" || pr.Id == "a")
+                {
+                    counterofA = counterofA + 1;
+                }
+                if (pr.Id == "B" || pr.Id == "b")
+                {
+                    counterofB = counterofB + 1;
+                }
+                if (pr.Id == "C" || pr.Id == "c")
+                {
+                    CounterofC = CounterofC + 1;
+                }
+                if (pr.Id == "D" || pr.Id == "d")
+                {
+                    CounterofD = CounterofD + 1;
+                }
+            }
+            int totalPriceofA = (counterofA / 3) * 130 + (counterofA % 3 * priceofA);
+            int totalPriceofB = (counterofB / 2) * 45 + (counterofB % 2 * priceofB);
+            int totalPriceofC = (CounterofC * priceofC);
+            int totalPriceofD = (CounterofD * priceofD);
+            return totalPriceofA + totalPriceofB + totalPriceofC + totalPriceofD;
+
+        }
+    }
+}
